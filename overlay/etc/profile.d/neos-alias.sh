fae14907255db0747e6cb43f7264ed0d2b3c535c
youssefx1x <yousefmzaky14@gmail.com>
2026-08-13 23:26:20 +0000

    config: update system files for consistency & compatibility
    - profile.d/neos-alias.sh: include the 'neo' umbrella entry point in the
      bash alias loop (all neos-* + neo + neolibs + pkg now have shortcuts)
    - gtk-3.0 + gtk-4.0 settings.ini: set Arc-Dark theme + Papirus-Dark icons +
      font so GTK apps are themed consistently outside the XFCE session too

--- overlay/etc/profile.d/neos-alias.sh
+++ overlay/etc/profile.d/neos-alias.sh
@@ -1,6 +1,6 @@
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 shopt -s expand_aliases
-for _f in /usr/bin/neos-* /usr/bin/neolibs /usr/bin/pkg; do
+for _f in /usr/bin/neos-* /usr/bin/neo /usr/bin/neolibs /usr/bin/pkg; do
   [ -x "$_f" ] && alias "$(basename "$_f")=/usr/bin/bash $_f"
 done
 unset _f
